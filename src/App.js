9e170d4d219c49edd24f1d23579adb5aaf0c935a
Ayesh Lakshan <ayeshlak1998@gmail.com>
2021-08-27 15:16:38 +0530

    Training Day 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Heading from "./components/heading";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App" style={{fontStyle: "italic", fontSize: "10px"}}>
+            <p className="test">
+                aaa
+            </p>
+            <Heading heading="heading 1"/>
+            <hr/>
+            <Heading heading="heading 2"/>
+            <hr/>
+            <Heading heading="heading 3"/>
+        </div>
+    );
 }
 
 export default App;
