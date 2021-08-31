5b8c5dd05fbce18c3624a45b7f6527eb68d6e89a
Ayesh Lakshan <ayeshlak1998@gmail.com>
2021-08-31 16:40:12 +0530

    Training Day 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import './App.css';
 
 import Heading from "./components/heading";
+import {useEffect, useState} from "react";
+
+
 
 function App() {
+
+    let heading = "heading"
+
+    const [name, setName] = useState("Kavindu");
+
+    const handleClick = () => {
+        setName("Imthath")
+    }
+
+    useEffect(() => {
+        console.log("useEffect executed")
+    }, [])
+
+    useEffect(() => {
+        console.log("useEffect for name")
+    }, [name])
+
+    // useEffect(() => {
+    //     return () => {
+    //         console.log("ended")
+    //     };
+    // }, []);
+
+
     return (
-        <div className="App" style={{fontStyle: "italic", fontSize: "10px"}}>
+        <div className="App" style={{ fontSize: "20px"}}>
             <p className="test">
                 aaa
             </p>
-            <Heading heading="heading 1"/>
+            <Heading heading={name} handleClick={handleClick}/>
             <hr/>
-            <Heading heading="heading 2"/>
+            <Heading heading={name} handleClick={handleClick}/>
             <hr/>
-            <Heading heading="heading 3"/>
+            <Heading heading={name} handleClick={handleClick}/>
         </div>
     );
 }
